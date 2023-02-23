016e7ca3545e8dff9c60ecd4c04241cfd09e6f75
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-02-23 18:09:41 +0300

    add: getstaticprops

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
-import { Layout } from '../../layout/Layout';
-import { Button, Htag, P, Tag, Rating } from 'components';
 import { useState } from 'react';
+import axios from 'axios';
+import { Layout } from '../../layout/Layout';
+import { Rating } from 'components';
+import { MenuItem } from 'interfaces/menu.interface';
 
-export default function Home(): JSX.Element {
+export default function Home({ menu, firstCategory }: HomeProps): JSX.Element {
 	const [rating, setRating] = useState<number>(5);
 	return (
 		<Layout>
@@ -13,18 +16,31 @@ export default function Home(): JSX.Element {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<Htag tag='h1'>text</Htag>
 
-			<Button appearance='primary' arrow='down'>
-				text
-			</Button>
-			<Button appearance='ghost' arrow='down'>
-				text
-			</Button>
-			<P fz='s'>asdasasdasd</P>
-			<P fz='m'>asdasasdasd</P>
-			<P fz='l'>asdasasdasd</P>
+			<ul>
+				{menu.map((m) => (
+					<li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+				))}
+			</ul>
 			<Rating rating={rating} setRating={setRating} isEditable />
 		</Layout>
 	);
+}
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+
+	const menu = axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', { firstCategory });
+
+	return {
+		props: {
+			menu,
+			firstCategory,
+		},
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
 }
