d63d4559860f54a884755d630881de767152db3b
dxxbletriiiple <dxxbletriiiple@gmail.com>
2024-01-24 16:04:26 +0300

    checkpoint

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import styles from './Footer.module.scss';
 export const Footer = ({ className, children }: FooterProps): JSX.Element => {
 	return (
 		<footer className={cn(className, styles.footer)}>
-			<div>OwlTop © 2020 - {new Date().getFullYear()} Все права защищены</div>
+			<div>dxxbletriiiple © 2020 - {new Date().getFullYear()} Все права защищены</div>
 			<a href='#!' target='_blank' rel='noopener noreferrer'>
 				Пользовательское соглашение
 			</a>
